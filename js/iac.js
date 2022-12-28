0b16ee6d5c05d122ecbb86a57267d8953c78504d
Daniel Vitorio <dvitorio2@gmail.com>
2022-12-28 11:16:10 -0300

    Ajustes nas funções do modal de IAC

--- js/iac.js
+++ js/iac.js
@@ -1,13 +1,15 @@
 //INICIALIZAÇÃÕ DAS VARIÁVEIS GLOBAIS
-const btnIAC = document.getElementById('btnIAC');
-const iacModal = document.getElementById('iacModal'); 
 const btnCleanIAC = document.querySelector('#btnCleanIAC');
 const btnCloseIAC = document.getElementById('btnCloseIAC');
-const iacResult = document.getElementById('iacResult');
-const inputIACResults = document.getElementById('inputIACResults');
+const btnIAC = document.getElementById('btnIAC');
+const btnPrintIAC = document.getElementById('btnPrintIAC');
+const iacBox = document.getElementById('iacBox');
 const iacButtonsResult = document.getElementById('iacButtonsResult');
-const iacGenderResult = document.getElementById('iacGenderResult');
-const iacSituationResult = document.getElementById('iacSituationResult');
+const iacModal = document.getElementById('iacModal'); 
+var iacGenderResult = document.getElementById('iacGenderResult');
+var iacResult = document.getElementById('iacResult');
+var iacSituationResult = document.getElementById('iacSituationResult');
+var inputIACResults = document.getElementById('inputIACResults');
 
 //Abre o modal de IAC
 btnIAC.addEventListener('click', ()=>{
@@ -19,47 +21,47 @@ btnIAC.addEventListener('click', ()=>{
 btnCalculateIAC.addEventListener('click', ()=>{
      var iac_gender = document.getElementsByName('genderIAC');
           for(var i = 0; i < iac_gender.length; i++){
-               let iacHeight = parseFloat(document.getElementById('iacHeight').value);
-               let iacHip = parseFloat(document.getElementById('iacHip').value);
-               let iac = (iacHip/(iacHeight * Math.pow(iacHeight, 0.5)) - 18).toFixed(2);
+               var iacHeight = parseFloat(document.getElementById('iacHeight').value.replace(',','.'));
+               var iacHip = parseFloat(document.getElementById('iacHip').value.replace(',','.'));
+               var iac = (iacHip/(iacHeight * Math.pow(iacHeight, 0.5)) - 18).toFixed(2);
                inputIACResults.classList.remove('d-none');
                iacButtonsResult.classList.remove('d-none');
                iacResult.value = iac;
-               if((iac_gender[i].checked && iac_gender[i].value == '1') && (iac > 8 && iac <= 20)){
+               if((iac_gender[i].checked && iac_gender[i].value == '1') && (iac > 8 && iac < 20)){
+                    iacResult.classList.add('text-success', 'font-weight-bold');
+                    iacSituationResult.classList.add('text-success');
                     iacGenderResult.value = "Sexo: Masculino"; 
                     iacSituationResult.value = "Situação: Adiposidade normal";
-                    iacResult.classList.add('text-success', 'font-weight-bold');
-                    iacSituationResult.classList.add('text-success');
                }
-               else if((iac_gender[i].checked && iac_gender[i].value == '1') && (iac > 20 && iac <= 25)){
-                    iacGenderResult.value = "Sexo: Masculino"; 
-                    iacSituationResult.value = "Situação: Sobrepeso";
+               if((iac_gender[i].checked && iac_gender[i].value == '1') && (iac > 20 && iac < 25)){
                     iacResult.classList.add('text-warning', 'font-weight-bold');
                     iacSituationResult.classList.add('text-warning'); 
+                    iacGenderResult.value = "Sexo: Masculino"; 
+                    iacSituationResult.value = "Situação: Sobrepeso";
                }
-               else if((iac_gender[i].checked && iac_gender[i].value == '1') && (iac > 25)){
+               if((iac_gender[i].checked && iac_gender[i].value == '1') && (iac > 25)){
+                    iacResult.classList.add('text-danger', 'font-weight-bold');
+                    iacSituationResult.classList.add('text-danger');
                     iacGenderResult.value = "Sexo: Masculino"; 
                     iacSituationResult.value = "Situação: Obesidade"; 
-                    iacResult.classList.add('text-danger', 'font-weight-bold');
-                    iacSituationResult.classList.add('text-danger');
                }
-               else if((iac_gender[i].checked && iac_gender[i].value == '2') && (iac > 21 && iac <= 32)){
-                    iacGenderResult.value = "Sexo: Feminino"; 
-                    iacSituationResult.value = "Situação: Adisposidade normal";
+               if((iac_gender[i].checked && iac_gender[i].value == '2') && (iac > 21 && iac < 32)){
                     iacResult.classList.add('text-success', 'font-weight-bold');
                     iacSituationResult.classList.add('text-success');
-               }
-               else if((iac_gender[i].checked && iac_gender[i].value == '2') && (iac > 32 && iac <= 38)){
                     iacGenderResult.value = "Sexo: Feminino"; 
-                    iacSituationResult.value = "Situação: Sobrepeso"; 
+                    iacSituationResult.value = "Situação: Adisposidade normal";
+               }
+               if((iac_gender[i].checked && iac_gender[i].value == '2') && (iac > 32 && iac < 38)){
                     iacResult.classList.add('text-warning', 'font-weight-bold');
                     iacSituationResult.classList.add('text-warning');
-               }
-               else if((iac_gender[i].checked && iac_gender[i].value == '2') && (iac > 38)){
                     iacGenderResult.value = "Sexo: Feminino"; 
-                    iacSituationResult.value = "Situação: Obesidade"; 
+                    iacSituationResult.value = "Situação: Sobrepeso"; 
+               }
+               if((iac_gender[i].checked && iac_gender[i].value == '2') && (iac > 38)){
                     iacResult.classList.add('text-danger', 'font-weight-bold');
                     iacSituationResult.classList.add('text-danger');
+                    iacGenderResult.value = "Sexo: Feminino"; 
+                    iacSituationResult.value = "Situação: Obesidade"; 
                }
           }
 });
@@ -72,6 +74,11 @@ btnCleanIAC.addEventListener('click', ()=>{
      iacGenderResult.value = '';
      iacSituationResult.value = '';
 });
+
+//Imprime os resulatdos do IAC
+btnPrintIAC.addEventListener('click', ()=>{
+     window.print();
+})
 
 //Fecha o modal de IAC
 btnCloseIAC.addEventListener('click', ()=>{
