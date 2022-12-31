5a7d598fd98a8025554e27221e2d1a2a6cd33b6b
Daniel Vitorio <dvitorio2@gmail.com>
2022-12-30 23:17:14 -0300

    Ajustes na função de TFG

--- js/tfg.js
+++ js/tfg.js
@@ -1,18 +1,32 @@
 //INICIALIZAÇÃO DAS VARIÁVEIS GLOBAIS
 const btnCalculateTFG = document.getElementById('btnCalculateTFG');
-var tfgGenderResultMan = document.getElementById('tfgGenderResultMan');
-var tfgGenderResult = document.getElementById('tfgGenderResult');
-var tfgSituationResult = document.getElementById('tfgSituationResult');
+const btnCleanTFG = document.getElementById('btnCleanTFG');
+const btnCloseTFG = document.getElementById('btnCloseTFG');
+const btnPrintTFG = document.getElementById('btnPrintTFG');
+const btnTFG = document.getElementById('btnTFG');
 const inputTFGResults = document.getElementById('inputTFGResults');
-var tfgResult = document.getElementById('tfgResult');
 const tfgButtonsResult = document.getElementById('tfgButtonsResult');
+const tfgModal = document.getElementById('tfgModal');
+var tfgGenderResult = document.getElementById('tfgGenderResult');
+var tfgGenderResultMan = document.getElementById('tfgGenderResultMan');
+var tfgResult = document.getElementById('tfgResult');
+var tfgSituationResult = document.getElementById('tfgSituationResult');
 
+
+
+//Abre o modal de TFG
+btnTFG.addEventListener('click', ()=>{
+     menuButtons.classList.add('d-none');
+     tfgModal.classList.remove('d-none');
+     tfgButtonsResult.classList.add('d-none');
+     inputTFGResults.classList.add('d-none');
+})
 
 //Calcula a TFG
 btnCalculateTFG.addEventListener('click', ()=>{
 
-     var tfg_man = document.getElementById('tfg_man').value;
-     var tfg_woman = document.getElementById('tfg_woman').value;
+     var tfg_man = document.getElementById('tfg_man').value; //Seleciona genero masculino
+     var tfg_woman = document.getElementById('tfg_woman').value; //Seleciona genero feminino
      var tfgCreatinine = parseFloat(document.getElementById('tfgCreatinine').value);
      var tfgWeight = parseFloat(document.getElementById('tfgWeight').value);
      var tfgAge = parseFloat(document.getElementById('tfgAge').value);
@@ -31,22 +45,22 @@ btnCalculateTFG.addEventListener('click', ()=>{
 
                if(tfgResultMan >= 90){
                     tfgGenderResult.classList.add('text-success');
-                    tfgSituationResult.value = 'TFG normal ou elevada';
+                    tfgSituationResult.value = 'Filtração normal ou elevada';
                     tfgSituationResult.classList.add('text-success');           
                }
                else if(tfgResultMan > 60 && tfgResultMan <= 89){
                     tfgGenderResult.classList.add('text-primary');
-                    tfgSituationResult.value = 'TFG com redução discreta'; 
+                    tfgSituationResult.value = 'Filtração com redução discreta'; 
                     tfgSituationResult.classList.add('text-primary');            
                }
                else if(tfgResultMan > 45 && tfgResultMan <= 59){
                     tfgGenderResult.classList.add('text-primary');
-                    tfgSituationResult.value = 'TFG com redução discreta a moderada'; 
+                    tfgSituationResult.value = 'Filtração com redução discreta a moderada'; 
                     tfgSituationResult.classList.add('text-primary');           
                }
                else if(tfgResultMan > 30 && tfgResultMan <= 44){
                     tfgGenderResult.classList.add('text-warning');
-                    tfgSituationResult.value = 'TFG com redução moderada ou severa';
+                    tfgSituationResult.value = 'Filtração com redução moderada ou severa';
                     tfgSituationResult.classList.add('text-warning');           
                }
                else if(tfgResultMan > 15 && tfgResultMan <= 29){
@@ -69,22 +83,22 @@ btnCalculateTFG.addEventListener('click', ()=>{
 
                if(tfgResultWoman >= 90){
                     tfgResult.classList.add('text-success');
-                    tfgSituationResult.value = 'TFG normal ou elevada';
+                    tfgSituationResult.value = 'Filtração normal ou elevada';
                     tfgSituationResult.classList.add('text-success');           
                }
                else if(tfgResultWoman > 60 && tfgResultWoman <= 89){
                     tfgResult.classList.add('text-primary');
-                    tfgSituationResult.value = 'TFG com redução discreta'; 
+                    tfgSituationResult.value = 'Filtração com redução discreta'; 
                     tfgSituationResult.classList.add('text-primary');            
                }
                else if(tfgResultWoman > 45 && tfgResultWoman <= 59){
                     tfgResult.classList.add('text-primary');
-                    tfgSituationResult.value = 'TFG com redução discreta a moderada'; 
+                    tfgSituationResult.value = 'Filtração com redução discreta a moderada'; 
                     tfgSituationResult.classList.add('text-primary');           
                }
                else if(tfgResultWoman > 30 && tfgResultWoman <= 44){
                     tfgResult.classList.add('text-warning');
-                    tfgSituationResult.value = 'TFG com redução moderada ou severa';
+                    tfgSituationResult.value = 'Filtração com redução moderada ou severa';
                     tfgSituationResult.classList.add('text-warning');           
                }
                else if(tfgResultWoman > 15 && tfgResultWoman <= 29){
@@ -100,4 +114,27 @@ btnCalculateTFG.addEventListener('click', ()=>{
                tfgResult.value = tfgResultWoman.toFixed(1);
           }
      }
+});
+
+//Limpa os campos do modal de TFG
+btnCleanTFG.onclick = ()=>{
+     document.getElementById('tfgGenderResult').value = '';
+     document.getElementById('tfgSituationResult').value = '';
+     document.getElementById('tfgResult').value = '';
+     document.getElementById('tfg_man').checked = false;
+     document.getElementById('tfg_woman').value = '';
+     document.getElementById('tfgCreatinine').value = '';
+     document.getElementById('tfgWeight').value = '';
+     document.getElementById('tfgAge').value = '';
+};
+
+//Imprime o resultado do TFG
+btnPrintTFG.addEventListener('click', ()=>{
+     window.print();
 })
+
+//Fecha o modal de TFG
+btnCloseTFG.addEventListener('click', ()=>{
+     tfgModal.classList.add('d-none');
+     menuButtons.classList.remove('d-none');
+});
